02eb2083ca424ee6a82380a5e97f1cd4658a6eba
Joel Meador <joel@expectedbehavior.com>
2018-08-03 02:01:38 -0400

    forgot to set the standard deviation correctly when computing variance, but not any more

--- Calculator.SimpleVariance.cs
+++ Calculator.SimpleVariance.cs
@@ -18,7 +18,12 @@ namespace WeaponRealizer
                 var damagePerShot = weapon.DamagePerShot;
                 var adjustment = rawDamage / damagePerShot;
                 var variance = weapon.weaponDef.DamageVariance;
-                var roll = NormalDistribution.Random(new VarianceBounds(damagePerShot - variance, damagePerShot + variance, Core.ModSettings.StandardDeviationSimpleVarianceMultiplier));
+                var roll = NormalDistribution.Random(
+                    new VarianceBounds(
+                        damagePerShot - variance,
+                        damagePerShot + variance,
+                        Core.ModSettings.StandardDeviationSimpleVarianceMultiplier * variance
+                    ));
                 var variantDamage = roll * adjustment;
             
                 var sb = new StringBuilder();
@@ -31,19 +36,6 @@ namespace WeaponRealizer
 
                 return variantDamage;
             }
-//        private static bool _doTestSimpleVarianceCalculated = false;
-//        private static void DoTestSimpleVarianceCalculations(Weapon weapon, float damage)
-//        {
-//            if (!_doTestSimpleVarianceCalculated) return;
-//
-//            var sb = new StringBuilder();
-//            for (var i = 0; i < 1000; i++)
-//            {
-//                sb.AppendLine(DoSimpleVarianceCalculate(weapon, damage).ToString());
-//            }
-//            _doTestSimpleVarianceCalculated = false;
-//            Logger.Debug(sb.ToString());
-//        }
         }
     }
 }
