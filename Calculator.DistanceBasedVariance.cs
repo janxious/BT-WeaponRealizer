0ee64821ec762ac14349aa272bd796c478ec79dd
Joel Meador <joel@expectedbehavior.com>
2018-08-04 22:13:00 -0400

    add tag based variation on damage by distance variance to control how quickly damage will cap out

--- Calculator.DistanceBasedVariance.cs
+++ Calculator.DistanceBasedVariance.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using BattleTech;
 using UnityEngine;
 
@@ -8,25 +10,24 @@ namespace WeaponRealizer
     {
         private static class DistanceBasedVariance
         {
-            private const string DistanceVarianceTag = "WR-variance_by_distance";
-            private const double Pi2 = Math.PI / 2.0;
-
             public static bool IsApplicable(Weapon weapon)
             {
                 return Core.ModSettings.DistanceBasedVariance &&
-                       weapon.weaponDef.ComponentTags.Contains(DistanceVarianceTag);
+                       HasPositiveVarianceMultiplier(weapon);
             }
 
-            public static float Calculate(AbstractActor attacker, ICombatant target, Weapon weapon, float damage, float rawDamage)
+            private const double Pi2 = Math.PI / 2.0;
+            private static readonly Dictionary<string, float> VarianceMultipliers = new Dictionary<string, float>();
+            public static float Calculate(ICombatant attacker, ICombatant target, Weapon weapon, float damage, float rawDamage)
             {
                 var damagePerShot = weapon.DamagePerShot;
                 var adjustment = rawDamage / damagePerShot;
-                var baseMultiplier = Core.ModSettings.DistanceBasedVarianceMaxRangeDamageMultiplier;
                 float varianceMultiplier;
                 var distance = Vector3.Distance(attacker.TargetPosition, target.TargetPosition);
                 var distanceDifference = weapon.MaxRange - distance;
                 var distanceRatio = distanceDifference / weapon.MaxRange;
-                var distanceBasedFunctionMultiplier = (float) Math.Atan(Pi2 * (distanceRatio) + baseMultiplier);
+                var baseMultiplier = VarianceMultipliers[weapon.defId];
+                var distanceBasedFunctionMultiplier = (float) Math.Atan(Pi2 * distanceRatio + baseMultiplier);
                 if (distance < weapon.MaxRange)
                 {
                     varianceMultiplier = Mathf.Max(
@@ -40,16 +41,40 @@ namespace WeaponRealizer
                 {
                     return damage;
                 }
-                Logger.Debug($"madness: {distanceBasedFunctionMultiplier}\n" +
+                var computedDamage = damage * varianceMultiplier * adjustment;
+                Logger.Debug($"distanceBasedFunctionMultiplier: {distanceBasedFunctionMultiplier}\n" +
+                             $"defId: {weapon.defId}\n" +
                              $"varianceMultiplier: {varianceMultiplier}\n" +
                              $"adjustment: {adjustment}\n" +
                              $"damage: {damage}\n" +
                              $"distance: {distance}\n" +
                              $"max: {weapon.MaxRange}\n" +
                              $"distanceDifference {distanceDifference}\n" +
-                             $"basemultplier: {baseMultiplier}\n" +
-                             $"distanceRatio: {distanceRatio}");
-                return damage * varianceMultiplier * adjustment;
+                             $"baseMultplier: {baseMultiplier}\n" +
+                             $"distanceRatio: {distanceRatio}\n" +
+                             $"computedDamage: {computedDamage}\n");
+                return computedDamage;
+            }
+
+            private static bool HasPositiveVarianceMultiplier(Weapon weapon)
+            {
+                if (!VarianceMultipliers.ContainsKey(weapon.defId))
+                    VarianceMultipliers[weapon.defId] = ParseBaseMultiplier(weapon);
+                return VarianceMultipliers[weapon.defId] > Epsilon;
+            }
+
+            private const string DistanceVarianceTagPrefix = "WR-variance_by_distance";
+            private static readonly char[] TagDelimiter = new char[] {'-'};
+            private static float ParseBaseMultiplier(Weapon weapon)
+            {
+                if (!weapon.weaponDef.ComponentTags.Any(tag => tag.StartsWith(DistanceVarianceTagPrefix)))
+                    return 0.0f;
+                var rawTag = weapon.weaponDef.ComponentTags.First(tag => tag.StartsWith(DistanceVarianceTagPrefix));
+                var baseMultiplier =
+                    rawTag == DistanceVarianceTagPrefix
+                        ? Core.ModSettings.DistanceBasedVarianceMaxRangeDamageMultiplier
+                        : float.Parse(rawTag.Split(TagDelimiter, 3).Last()) / 100.0f;
+                return baseMultiplier;
             }
         }
     }
